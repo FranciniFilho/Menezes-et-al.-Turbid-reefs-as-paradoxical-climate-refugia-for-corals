5e6b4ffff6255c342135110790e97d082adf7256
FranciniFilho <francinifilho@usp.br>
2026-01-14 10:15:58 -0300

    before health/interaction PCA viz refactoring

--- #######FINAL_CODES/test_pdf.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)
-p <- ggplot(mtcars, aes(wt, mpg)) +
-    geom_point() +
-    labs(title = "Test PDF")
-dir <- "c:/Users/rbfra/OneDrive/########PUBLICACOES/############Menezes et al. Mus his distribution and abundance Abrolhos/######FINAL/#######FINAL_RESULTS/PCA_Publication_Figures"
-dir.create(dir, showWarnings = FALSE, recursive = TRUE)
-ggsave(file.path(dir, "test_output.pdf"), p)
-cat("PDF saved successfully\n")
